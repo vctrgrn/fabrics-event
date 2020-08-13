987ea12cdcbbb2ca16bb2abbf8cb8792824edc4b
Victor Guerinon <victor@macbook-pro-de-victor.home>
2020-08-13 12:13:39 +0200

    recuperation des produits par categorie (appel de service), evolution du mock db.json

--- src/app/listeProduits/page-liste-produits/page-liste-produits.component.ts
+++ src/app/listeProduits/page-liste-produits/page-liste-produits.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ProduitService } from 'src/app/shared/service/produit.service';
 
 @Component({
   selector: 'app-page-liste-produits',
@@ -8,12 +9,29 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class PageListeProduitsComponent implements OnInit {
   categorie;
+  listProduitByCategorie = []
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private produitService: ProduitService) { }
 
   ngOnInit(): void {
    this.categorie = this.route.snapshot.paramMap.get('categorie');
    console.log(this.categorie)
+    this.produitService.getProduitByCategorie().subscribe(
+      data => {
+          this.preparerDonneesPourAffichage(data);
+      },
+      error => {
+        console.log(error);
+      }
+    )
+  }
+
+  preparerDonneesPourAffichage(data) {
+    for(let produit of data) {
+      if(produit.id_Categorie == this.categorie) {
+        this.listProduitByCategorie.push(produit);
+      }
+    }
   }
 
 }
