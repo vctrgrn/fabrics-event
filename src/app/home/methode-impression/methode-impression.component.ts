6c2f29400700ecc65170c0b5423bc800ce015be3
Victor Guerinon <victor@macbook-pro-de-victor.home>
2020-08-15 22:41:46 +0200

    changement image methode impression

--- src/app/home/methode-impression/methode-impression.component.ts
+++ src/app/home/methode-impression/methode-impression.component.ts
@@ -6,37 +6,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./methode-impression.component.scss']
 })
 export class MethodeImpressionComponent implements OnInit {
+  urlImage;
+  imagePath='../../../assets/images/'
    methodeImpressionTop = [
     {
       'titre': 'flocage',
-      'text': 'Motif imprimé sur une feuille, découpé puis pressé à chaud sur le textile. Idéal pour de petites quantités.'
+      'text': 'Motif imprimé sur une feuille, découpé puis pressé à chaud sur le textile. Idéal pour de petites quantités.',
+      'image': 'flex.png'
     }, 
     {
       'titre': 'flex',
-      'text' : 'Motif imprimé sur une feuille, découpé puis pressé à chaud sur le textile. Couleurs plus resplendissantes, colle appliquée sur le rebord de l’impression. Idéal pour une quantité moyenne.'
+      'text' : 'Motif imprimé sur une feuille, découpé puis pressé à chaud sur le textile. Couleurs plus resplendissantes, colle appliquée sur le rebord de l’impression. Idéal pour une quantité moyenne.',
+      'image': 'flocage.png'
       
     }, 
     {
       'titre': 'serigraphie',
-      'text' : 'cadres et pochoirs. L’encre passe à travers le pochoir et se dépose sur le textile ensuite. Les couleurs sont moins resplendissantes qu’une impression numérique mais tiennent très bien avec le temps. Ce procédé reste'
+      'text' : 'cadres et pochoirs. L’encre passe à travers le pochoir et se dépose sur le textile ensuite. Les couleurs sont moins resplendissantes qu’une impression numérique mais tiennent très bien avec le temps. Ce procédé reste',
+      'image': 'flex.png'
     }
   ]
 
   methodeImpressionBottom = [
     {
       'titre': 'broderie',
-      'text' : 'Motif réalisé à l’aiguille ou à la machine, et ce directement sur le support concerné. Haute qualité.'
+      'text' : 'Motif réalisé à l’aiguille ou à la machine, et ce directement sur le support concerné. Haute qualité.',
+      'image': 'flex.png'
     }
     , 
     {
       'titre': 'impression numérique',
-      'text' : 'Motif imprimé directement sur le textile avec l’aide d’imprimantes. Malgré un système de personnalisation de haute qualité et un résultat optimal, les couleurs durent moins dans le temps à la différence de la sérigraphie.'
+      'text' : 'Motif imprimé directement sur le textile avec l’aide d’imprimantes. Malgré un système de personnalisation de haute qualité et un résultat optimal, les couleurs durent moins dans le temps à la différence de la sérigraphie.',
+      'image': 'flocage.png'
     }
   ]
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getImage(image): string {
+    return  this.imagePath + image;
   }
 
 }
